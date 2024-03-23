963bce7dd2e6f9cde3f88f7d3c2bdf0e6e49edea
Snowlance <snowlance7@gmail.com>
2024-03-22 19:09:15 -0500

    bug should be fixed now

--- Patches/MoveToShipPatch.cs
+++ Patches/MoveToShipPatch.cs
@@ -7,38 +7,26 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using LC_NoOneLeftBehindAlt;
 
-namespace LC_NoOneLeftBehindAlt
+namespace LC_NoOneLeftBehindAlt.Patches
 {
     [HarmonyPatch]
     internal static class MoveToShipPatch
     {
         private static readonly MethodInfo tpPlayer = typeof(StartOfRound).GetMethod("TeleportPlayerInShipIfOutOfRoomBounds", BindingFlags.Instance | BindingFlags.NonPublic);
-        private static bool aparatusTaken = false;
-        private static ManualLogSource Logger { get; set; }
-
-        public static void Init(ManualLogSource logger)
-        {
-            Logger = logger;
-        }
-
-        [HarmonyPatch(typeof(LungProp), nameof(LungProp.EquipItem))]
-        [HarmonyPrefix]
-        public static void ApparatusTaken(StartOfRound __instance)
-        {
-            aparatusTaken = true;
-        }
+        private static ManualLogSource Logger = Plugin.LoggerInstance;
 
         [HarmonyPatch(typeof(StartOfRound), "ShipLeave")]
         [HarmonyPrefix]
         public static void TpOnShipLeave(StartOfRound __instance)
         {
-            if (!aparatusTaken)
+            Logger.LogDebug($"Aparatus Taken: {Plugin.Instance.aparatusTaken}");
+            if (!Plugin.Instance.aparatusTaken)
             {
-                Logger.LogInfo((object)"Teleporting player back to the ship...");
+                Logger.LogInfo("Teleporting player back to the ship...");
                 tpPlayer?.Invoke(__instance, null);
             }
-            aparatusTaken = false;
         }
     }
 }
